9533c39de39d9a700fdd506d05bb43c7c882cce1
Andrius Burnelis <andrius.burn@gmail.com>
2024-04-17 13:28:26 -0400

    Added numba

---
+++ sim_pt_separate_base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 17 13:19:51 EDT 2024
+
+@author: Andrius Burnelis
+"""
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
